104350b29ee74fd3c5b7c2ba61226be36b80f907
RyanAdamson1 <ryan.adamson9300@protonmail.com>
2026-08-19 12:17:27 -0400

    main update with copy from example to catch up

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+"""The main web app for my cool new service"""
+
+import json
+import os
+from datetime import datetime, timezone
+
 import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-def root():
+
 @app.get("/")
-async def index():
-   return {"message": "Hello World"}
+def main():
+    """The root endpoint as a testbed"""
+    return {"hello": "world"}
+
+
+@app.post("/convert/{state}/{city}")
+def convert(state, city):
+    """Converts a city/state combo to lat/long"""
+    data = {"api_key": "6a7f57138e04b429290986wxia11d90", "city": city, "state": state}
+    response = requests.get("https://geocode.maps.co/search", params=data, timeout=3)
+    print(response.json())
+    return {"lat": response.json()[0]["lat"], "long": response.json()[0]["lon"]}
+
